7df216c39db5e3fce678facd055a8a7aa129a892
范智凝 <xn_fanzhining@163.com>
2014-08-27 17:24:57 +0800

    modify

--- src/main/java/com/energyying/tc/sample/TeamCity.java
+++ src/main/java/com/energyying/tc/sample/TeamCity.java
@@ -6,8 +6,10 @@ package com.energyying.tc.sample;
 public class TeamCity {
 
     public Long getLong(String input){
-        if(input == ""){
+        if(input.equals("")){
             return 1L;
+        }else if (input.equals("123")){
+            return new Long("321");
         }else{
             return new Long(input);
         }
